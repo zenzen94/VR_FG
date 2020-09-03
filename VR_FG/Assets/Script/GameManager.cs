941ae38a6163c8c6ee909980cd256a76f55f65bd
zenzen <jasen0324@naver.com>
2020-09-03 20:42:55 +0900

    포톤 튜토리얼 3

---
+++ VR_FG/Assets/Script/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Script
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        private static GameManager instance;
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var obj = FindObjectOfType<GameManager>();
+                    if (obj != null)
+                    {
+                        instance = obj;
+                    }
+                    else
+                    {
+                        var newSingleton = new GameObject("Launcher").AddComponent<GameManager>();
+                        instance = newSingleton;
+                    }
+                }
+                return instance;
+            }
+
+            private set
+            {
+                instance = value;
+            }
+        }
+
+        private void Awake()
+        {
+            var objs = FindObjectsOfType<GameManager>();
+
+            if (objs.Length != 1)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+}
---
+++ VR_FG/Assets/Script/RoomPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script
+{
+    public class RoomPage : MonoBehaviour
+    {
+        public Button leaveRoomButton;
+
+        private void Awake()
+        {
+            leaveRoomButton.onClick.AddListener(OnLeaveRoomButtonClicked);
+        }
+
+        private void OnLeaveRoomButtonClicked()
+        {
+            Debug.Log("OnLeaveRoomButtonClicked");
+            GameManager.Instance.LeaveRoom();
+        }
+    }
+}
